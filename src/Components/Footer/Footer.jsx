40e9cf9a36ae8a06df6fa69973c52068fd2f420e
mayowa86 <mayowasamuel86@gmail.com>
2022-04-22 12:00:57 +0100

    footer section

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -3,47 +3,6 @@ import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
 
 import "./footer.css";
 
-const footerQuickLinks = [
-  {
-    display: "Home",
-    url: "#",
-  },
-  {
-    display: "About US",
-    url: "#",
-  },
-
-  {
-    display: "Courses",
-    url: "#",
-  },
-
-  {
-    display: "Blog",
-    url: "#",
-  },
-];
-
-const footerInfoLinks = [
-  {
-    display: "Privacy Policy",
-    url: "#",
-  },
-  {
-    display: "Membership",
-    url: "#",
-  },
-
-  {
-    display: "Purchases Guide",
-    url: "#",
-  },
-
-  {
-    display: "Terms of Service",
-    url: "#",
-  },
-];
 
 const Footer = () => {
   return (
@@ -52,71 +11,30 @@ const Footer = () => {
         <Row>
           <Col lg="3" md="6" className="mb-4">
             <h2 className=" d-flex align-items-center gap-1">
-              <i class="ri-pantone-line"></i> Learners.
+              <i class="ri-pantone-line"></i> Snaplearn
             </h2>
 
             <div className="follows">
-              <p className="mb-0">Follow us on social media</p>
-              <span>
-                {" "}
-                <a href="facebook.com">
-                  <i class="ri-facebook-line"></i>
-                </a>
-              </span>
-
-              <span>
-                {" "}
-                <a href="facebook.com">
-                  <i class="ri-instagram-line"></i>
-                </a>
-              </span>
-
-              <span>
-                {" "}
-                <a href="facebook.com">
-                  <i class="ri-linkedin-line"></i>
-                </a>
-              </span>
-
-              <span>
-                {" "}
-                <a href="facebook.com">
-                  <i class="ri-twitter-line"></i>
-                </a>
-              </span>
+              <small className="mb-0">New Africa Platform</small>              
             </div>
           </Col>
 
-          <Col lg="3" md="6" className="mb-4">
-            <h6 className="fw-bold">Explore</h6>
-            <ListGroup className="link__list">
-              {footerQuickLinks.map((item, index) => (
-                <ListGroupItem key={index} className="border-0 ps-0 link__item">
-                  {" "}
-                  <a href={item.url}>{item.display}</a>
-                </ListGroupItem>
-              ))}
-            </ListGroup>
+          <Col lg="3" md="6" className="explore">
+            <a href="#">
+            <h5 className="fw-bold">Term of use</h5>
+            </a>       
           </Col>
-
           <Col lg="3" md="6" className="mb-4">
-            <h6 className="fw-bold">Information</h6>
-            <ListGroup className="link__list">
-              {footerInfoLinks.map((item, index) => (
-                <ListGroupItem key={index} className="border-0 ps-0 link__item">
-                  {" "}
-                  <a href={item.url}>{item.display}</a>
-                </ListGroupItem>
-              ))}
-            </ListGroup>
+            <a href="#">
+            <h5 className="fw-bold">Privacy Policy</h5>
+            </a>           
           </Col>
-
           <Col lg="3" md="6">
-            <h6 className="fw-bold">Get in Touch</h6>
-
-            <p>Address: Sylhet, Bangladesh</p>
-            <p> Phone: +88 0123456789 </p>
-            <p>Email: example@gmail.com</p>
+            <a href="#">
+            <h5 className="fw-bold">Get in Touch</h5>
+            </a>
+            <p>Address: Lagos Nigeria</p>            
+            <p>Email: Snaplearn@newafrica.com</p>
           </Col>
         </Row>
       </Container>
